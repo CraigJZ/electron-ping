bbbc95bff919584da546f7f97b65559be5a4f4be
Craig Zaccaro <czaccaro@live.com>
2017-08-26 10:36:09 -0400

    ping google

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const {app, Menu, Tray} = require('electron')
 const notify = require('electron-main-notification')
+var ping = require('ping');
 
 let tray = null
 app.on('ready', () => {
   tray = new Tray('tray_icon_black.png')
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'click', click(){console.log('clicked the button')}},
-    {label: 'changecolor', click(){tray.setImage('tray_icon_purple.png')}},
+    {label: 'Change Icon Color to Purple', click(){tray.setImage('tray_icon_purple.png')}},
+    {label: 'Change Icon Color to Black', click(){tray.setImage('tray_icon_black.png')}},
     {role: 'quit'}
   ])
-  tray.setToolTip('This is my application.')
+  tray.setToolTip('Electron Ping')
   tray.setContextMenu(contextMenu)
 })
 
-
 app.on('ready', () => {
-  notify('This is a notification!', { body: 'See? Really easy to use!' }, () => {
+  notify('This is a notification!', { body: 'See? Really easy to use!', silent: true}, () => {
     console.log('The notification got clicked on!')
   })
 })
+
+var hosts = ['google.com'];
+
+hosts.forEach(function (host) {
+    ping.promise.probe(host)
+        .then(function (res) {
+            console.log(res);
+        });
+});
