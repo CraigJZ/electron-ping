4ecd144a2fe8f3774a190929af03a229717a1052
Craig Zaccaro <czaccaro@live.com>
2017-08-26 09:13:26 -0400

    tray icon and menu

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.on('ready', () => {
   tray = new Tray('tray_icon_black.png')
   const contextMenu = Menu.buildFromTemplate([
     {role: 'quit'},
+    {label: 'click', click(){console.log('clicked the button')}},
+    {label: 'changecolor', click(){tray.setImage('tray_icon_purple.png')}}
   ])
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
