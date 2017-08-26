d13966b73b02e57ab551f621ec8a39b277cddee9
Craig Zaccaro <czaccaro@live.com>
2017-08-26 09:41:59 -0400

    Notifications from electron

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const {app, Menu, Tray} = require('electron')
+const notify = require('electron-main-notification')
 
 let tray = null
 app.on('ready', () => {
   tray = new Tray('tray_icon_black.png')
   const contextMenu = Menu.buildFromTemplate([
-    {role: 'quit'},
     {label: 'click', click(){console.log('clicked the button')}},
-    {label: 'changecolor', click(){tray.setImage('tray_icon_purple.png')}}
+    {label: 'changecolor', click(){tray.setImage('tray_icon_purple.png')}},
+    {role: 'quit'}
   ])
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
+})
+
+
+app.on('ready', () => {
+  notify('This is a notification!', { body: 'See? Really easy to use!' }, () => {
+    console.log('The notification got clicked on!')
+  })
 })
